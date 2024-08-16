748dcb16b6281047e5ddcf6f75bad5e6e21ada8c
dc950 <dac95@hotmail.co.uk>
2024-08-17 00:11:21 +0100

    Bullet Collision Collection

--- scenes/tower/bullet.gd
+++ scenes/tower/bullet.gd
@@ -1,14 +1,33 @@
-extends RigidBody2D
+extends Area2D
 
 class_name Bullet
 
 @export var speed: float = 200.0
+@export var damage : float = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	#look_at(position.direction_to(target))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_and_collide(Vector2.RIGHT.rotated(rotation) * speed * delta)
+	position += (Vector2.RIGHT.rotated(rotation) * speed * delta)
+
+func _on_body_entered(body: Node) -> void:
+	print("Im in something")
+	if body.has_method("damage"):
+		print("dmg")
+		var attack : Attack = Attack.new()
+		attack.damage = damage
+		body.damage(attack)
+	else:
+		print("no damage")
+	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Ive hit something")
+	if area.has_method("damage"):
+		var attack : Attack = Attack.new()
+		attack.damage = damage
+		area.damage(attack)

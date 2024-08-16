97b39ce1e72c324a0a6f9658bc28883babd71470
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-08-16 22:52:11 +0100

    Tower Body Detection

--- scenes/tower/tower.gd
+++ scenes/tower/tower.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 var bullet = preload("res://scenes/tower/bullet.tscn")
 @export var test_target : Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +15,11 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_timer_timeout() -> void:
-	var new_bullet : Bullet = bullet.instantiate()
-	add_child(new_bullet)
-	new_bullet.look_at(test_target.position)
+	var collisions = get_overlapping_bodies()
+	if collisions.size() >= 1:
+		var new_bullet : Bullet = bullet.instantiate()
+		add_child(new_bullet)
+		new_bullet.look_at(collisions[0].position)
+	else:
+		print("no bodies")
 	

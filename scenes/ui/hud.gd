e5f02a53f07a57a03704973bd69383225ee8e38d
Nicholas Robinson <kylenr23@gmail.com>
2024-08-17 17:16:33 +0100

    UPDATE cleanup debug statements and old code

--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass	
+	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	# Mouse in viewport coordinates.

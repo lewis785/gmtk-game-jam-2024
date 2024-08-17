4ee889464b01d932f3133e1e0ec1bbd9229fc832
Insay <guleninsay@gmail.com>
2024-08-17 17:10:03 +0100

    Add rat movement animation

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -16,7 +16,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
 func _physics_process(delta):
 	var direction = Vector3()
 	
@@ -29,22 +29,7 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
-## Set position, direction and velocity of Enemy movement
-#func _set_movement(spawn_location : PathFollow2D):
-	## Choose a random location to spawn.
-	#var enemy_spawn_location = spawn_location
-	##enemy_spawn_location.progress_ratio = randf()
-	#
-	## Set the enemy's direction perpendicular to the path direction.
-	#var direction = enemy_spawn_location.rotation 
-#
-	## Set the enemy's position to a random location
-	#position = enemy_spawn_location.position
-	#
-	## Choose the velocity for the enemy.
-	#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	#linear_velocity = velocity.rotated(direction)
-	
+
 func _on_health_component_damaged(damage):
 	print("Enemy hit")
 

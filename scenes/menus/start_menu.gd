9551836cbd3b3489e3cda7b605aa8aa0562dbd36
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-08-17 01:20:56 +0100

    ADD start menu

---
+++ scenes/menus/start_menu.gd
@@ -0,0 +1,9 @@
+extends ColorRect
+
+class_name StartMenu
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/environment/level.tscn")
+
+func _on_how_to_button_pressed() -> void:
+	pass # Replace with function body.

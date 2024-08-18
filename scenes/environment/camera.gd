5bd61fa89172eb0f97c2e0590fcf083af31425b9
Nicholas Robinson <kylenr23@gmail.com>
2024-08-18 02:09:42 +0100

    UPDATED camera starts in the middle of the level

--- scenes/environment/camera.gd
+++ scenes/environment/camera.gd
@@ -3,6 +3,13 @@ extends Camera2D
 @export var speed: int = 10
 @export var zoom_speed: int = 1
 
+@export var level : Level
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var resolution = level.level_size * 64
+	position = resolution/2
+
 func _input(event):
 	var movement = Vector2(0,0)
 	if event.is_action_pressed("ZoomCamIn"):

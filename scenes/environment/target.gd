527720eccb88b557038fc356fb852dc8e14b0f65
Nicholas Robinson <kylenr23@gmail.com>
2024-08-18 01:59:05 +0100

    ADD Lose game when Crystal is destroyed

--- scenes/environment/target.gd
+++ scenes/environment/target.gd
@@ -3,3 +3,19 @@ extends Node2D
 class_name Target
 
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+func _on_health_component_damaged(damage):
+	# Play damage sound
+	pass
+
+func _on_health_component_died():
+	animated_sprite_2d.play("die")
+	# Play Game Over sound
+	# End Game
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite_2d.animation == "die":
+		get_tree().quit() 

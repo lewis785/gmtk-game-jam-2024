2b85e439f592e8b8e215ea2e40a7f6b10238a743
Nicholas Robinson <kylenr23@gmail.com>
2024-08-18 21:03:41 +0100

    UPDATE Pause script

--- scenes/menus/pause_menu.gd
+++ scenes/menus/pause_menu.gd
@@ -8,18 +8,21 @@ class_name PauseMenu
 @onready var paused : bool = false
 
 func _ready():
-	music_volume.value = AudioManager.music_volume
 	fx_volume.value = AudioManager.fx_volume
-
-func _on_music_volume_value_changed(value: float) -> void:
-	AudioManager.set_music_volume(value)
+	music_volume.value = AudioManager.music_volume
 
 func _on_fx_volume_value_changed(value: float) -> void:
 	AudioManager.set_fx_volume(value)
 
+func _on_music_volume_value_changed(value: float) -> void:
+	AudioManager.set_music_volume(value)
+
 func _input(event):
 	if event.is_action_pressed("PauseToggle"):
 		toggle_pause()
+
+func _on_resume_button_pressed():
+	toggle_pause()
 		
 func toggle_pause():
 	if paused:
@@ -28,7 +31,3 @@ func toggle_pause():
 		paused = true
 	visible = paused
 	get_tree().paused = paused
-
-
-func _on_button_pressed():
-	toggle_pause()

5754d02964cca6c8a4d99b1fba514b94e8768743
Nicholas Robinson <kylenr23@gmail.com>
2024-08-18 17:42:38 +0100

    ADD sounds to Crystal for Hit and Death

--- scenes/environment/target.gd
+++ scenes/environment/target.gd
@@ -4,17 +4,17 @@ class_name Target
 
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var audio_stream_player_hit = $AudioStreamPlayerHit
+@onready var audio_stream_player_death = $AudioStreamPlayerDeath
 
 signal end_game
 
 func _on_health_component_damaged(damage):
-	# Play damage sound
-	pass
+	audio_stream_player_hit.play()
 
 func _on_health_component_died():
+	audio_stream_player_death.play()
 	animated_sprite_2d.play("die")
-	# Play Game Over sound
-
 
 func _on_animated_sprite_2d_animation_finished():
 	if animated_sprite_2d.animation == "die":

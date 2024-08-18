9b151e4e569a67187ca3931ca918d96b6e3c6759
Nicholas Robinson <kylenr23@gmail.com>
2024-08-18 22:15:07 +0100

    ADD Face Turret Audio & Beam

---
+++ scenes/tower/beam.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+class_name Beam
+
+@export var speed: float = 200.0
+@export var damage : float = 20.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += (Vector2.RIGHT.rotated(rotation) * speed * delta)
+
+func _on_area_entered(body: Area2D) -> void:
+	if body is Hitbox:
+		var attack : Attack = Attack.new()
+		attack.damage = damage
+		body.damage(attack)
+		queue_free()
---
+++ scenes/tower/face_tower.gd
@@ -0,0 +1,26 @@
+extends BaseTower
+
+var beam = preload("res://scenes/tower/beam.tscn")
+@export var test_target : Sprite2D
+@export var number_of_simultanious_shots = 4
+
+@onready var attack_range = $AttackRange
+@onready var audio_stream_player_2d_attack = $AudioStreamPlayer2DAttack
+
+@onready var died = false
+
+func _on_timer_timeout() -> void:
+	if !died:
+		var collisions: Array[Area2D] = attack_range.get_overlapping_areas()
+		if collisions.size() > 0:
+			audio_stream_player_2d_attack.play()
+		for i in range(0, min(collisions.size(), number_of_simultanious_shots)):
+			var new_beam : Beam = beam.instantiate()
+			add_child(new_beam)
+			new_beam.look_at(collisions[i].global_position)
+			new_beam.scale = Vector2(tower_scale, tower_scale)
+			new_beam.damage = damage
+
+func _on_health_component_died() -> void:
+	died = true
+	queue_free()

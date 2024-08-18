a15fd115a8c86372be42e10f4c8858b862aaff14
dc950 <dac95@hotmail.co.uk>
2024-08-18 15:29:57 +0100

    New Camera Zooming

--- scenes/environment/camera.gd
+++ scenes/environment/camera.gd
@@ -2,6 +2,24 @@ extends Camera2D
 
 class_name Camera
 
+# Lower cap for the `_zoom_level`.
+@export var min_zoom : float = 0.5
+# Upper cap for the `_zoom_level`.
+@export var max_zoom : float = 4.0
+# Controls how much we increase or decrease the `_zoom_level` on every turn of the scroll wheel.
+@export var zoom_factor : float = 0.1
+# Duration of the zoom's tween animation.
+@export var zoom_duration : float = 0.2
+
+# The camera's target zoom level.
+var _zoom_level : float = 1.0:
+	set(value):
+		_zoom_level = clamp(value, min_zoom, max_zoom)
+		tween_zoom()
+
+# We store a reference to the scene's tween node.
+var tween: Tween = create_tween()
+
 @export var speed: int = 10
 @export var zoom_speed: int = 1
 
@@ -15,14 +33,11 @@ func _ready():
 	position = resolution/2
 
 func _input(event):
-	var movement = Vector2(0,0)
 	if event.is_action_pressed("ZoomCamIn"):
-		movement += Vector2(zoom_speed,zoom_speed)
+		_zoom_level += zoom_factor
 	if event.is_action_pressed("ZoomCamOut"):
-		if zoom > Vector2(1,1):
-			movement -= Vector2(zoom_speed,zoom_speed)
-	zoom = zoom + movement
-
+		_zoom_level -= zoom_factor
+	print("zoom level: " + str(_zoom_level))
 
 func _process(_delta):
 	var movement = Vector2(0,0)
@@ -39,3 +54,12 @@ func _process(_delta):
 	if (position.y < 0 and movement.y < 0) or (position.y > resolution.y and movement.y > 0):
 		movement.y = 0
 	position = position + movement
+
+func tween_zoom():
+	zoom = Vector2(_zoom_level, _zoom_level)
+	#tween.tween_property(
+		#self,
+		#"zoom",
+		#Vector2(_zoom_level, _zoom_level),
+		#zoom_duration
+	#)

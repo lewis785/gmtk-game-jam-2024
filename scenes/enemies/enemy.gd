e63f08317e0563687dcadcbf36a4487303236b52
Insay <guleninsay@gmail.com>
2024-08-18 14:42:15 +0100

    Fix rat Enemy inheritance

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -1,94 +1,15 @@
-extends CharacterBody2D
+extends Enemy
 
-class_name Enemy
-
-@onready var health_component : HealthComponent = $HealthComponent
-@onready var nav : NavigationAgent2D = $NavigationAgent2D
-@onready var attack_timer: Timer = $AttackTimer
-@onready var hitbox: Hitbox = $Hitbox
-@onready var col: CollisionShape2D = $CollisionShape2D
-@onready var animated_sprite_2d = $AnimatedSprite2D
-
-@export var speed = 300
-@export var accel = 7
-@export var damage = 5
-
-var dead = false
+class_name Rat
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	super()
+	
+	# Set basic attributes
+	speed = 300
+	acceleration = 7
+	damage = 5
 	
 func _physics_process(delta):
-	if dead:
-		return
-	
-	if nav.target_position:
-		var direction = Vector3()
-		direction = nav.get_next_path_position() - global_position
-		direction = direction.normalized()
-		
-		velocity = velocity.lerp(direction * speed, accel * delta)
-		
-		move_and_slide()
-		set_sprite_direction(direction)
-
-func set_target(target : Node2D):
-	nav.target_position = target.global_position
-	
-
-func set_sprite_direction(direction: Vector2):
-	 # Check if there is significant movement
-	if animated_sprite_2d.animation == "move_right" or animated_sprite_2d.animation == "move_left" or animated_sprite_2d.animation ==  "move_down" or animated_sprite_2d.animation ==  "move_up":
-		if direction.length() > 0.3:
-			if abs(direction.x) > abs(direction.y):
-				# Horizontal movement
-				hitbox.set_rotation(PI/2)
-				col.set_rotation(PI/2)
-				
-				if direction.x > 0:
-					animated_sprite_2d.play("move_right")
-				else:
-					animated_sprite_2d.play("move_left")
-			else:
-				# Vertical movement
-				hitbox.set_rotation(PI)
-				col.set_rotation(PI)
-				
-				if direction.y > 0:
-					animated_sprite_2d.play("move_down")
-				else:
-					animated_sprite_2d.play("move_up")
-
-func _on_health_component_damaged(damage):
-	pass
-
-func _on_health_component_died():
-	if !dead:
-		dead = true
-		animated_sprite_2d.play("die")
-
-
-func _on_hitbox_area_entered(body: Node2D) -> void:
-	if !dead:
-		if body is Hitbox:
-			attack_timer.start()
-
-
-func _on_attack_timer_timeout() -> void:
-	if !dead:
-		var bodies = hitbox.get_overlapping_areas()
-		for body in bodies:
-			if body is Hitbox:
-				_deal_attack_damage(body)
-				return
-	attack_timer.stop()
-	
-func _deal_attack_damage(body: Hitbox):
-	var attack = Attack.new()
-	attack.damage = damage
-	body.damage(attack)
-
-func _on_animated_sprite_2d_animation_finished():
-	if animated_sprite_2d.animation == "die":
-		queue_free()
+	super(delta)

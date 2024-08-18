10fc7164550c43f88e179da4471caa0ee7201365
dc950 <dac95@hotmail.co.uk>
2024-08-18 20:48:46 +0100

    Make Bombs Explode

--- scenes/tower/bomb.gd
+++ scenes/tower/bomb.gd
@@ -7,8 +7,12 @@ class_name Bomb
 @export var speed: int = 200
 @export var target: Vector2
 
+var exploding = false
+
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
 @onready var blast_area: Area2D = $PathFollow2D/Area2D/BlastRadius
+@onready var gpu_particles_2d: GPUParticles2D = %GPUParticles2D
+@onready var sprite_2d: Sprite2D = %Sprite2D
 
 func _ready() -> void:
 	curve.set_point_out(0, Vector2(target.x / 2, -500))
@@ -16,6 +20,8 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	if not target: return
+	if exploding: 
+		return
 	if path_follow_2d.progress_ratio >= 0.98:
 		detonate()
 	path_follow_2d.progress += speed * delta
@@ -24,8 +30,13 @@ func detonate():
 	var all_targets = blast_area.get_overlapping_areas()
 	var attack = Attack.new()
 	attack.damage = damage
-	
+
 	for target in all_targets:
 		if target is Hitbox && target.get_parent().is_in_group("enemies"):
 			target.damage(attack)
+		
+	gpu_particles_2d.emitting = true
+	exploding = true
+	sprite_2d.visible = false
+	await get_tree().create_timer(1).timeout
 	queue_free()

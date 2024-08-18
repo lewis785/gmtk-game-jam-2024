366625d15f6a7f7e3bfdd948ab5a2bd1ccfbd958
Nicholas Robinson <kylenr23@gmail.com>
2024-08-18 01:06:13 +0100

    ADD Level Size accesible from Level node

--- scenes/environment/level.gd
+++ scenes/environment/level.gd
@@ -3,6 +3,10 @@ extends Area2D
 class_name Level
 
 @onready var target : Target = $Target
+@onready var base_tile_map_layer : TileMapLayer = $BaseTileMapLayer
+
+@export var level_size : Vector2 = Vector2()
 
 func _on_area_exited(area: Area2D) -> void:
 	area.queue_free()
+	

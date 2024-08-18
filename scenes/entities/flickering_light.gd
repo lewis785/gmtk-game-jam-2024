5bbcdd7ea831e8564e582809fc60728795bd9755
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-08-18 23:14:30 +0100

    ADD flicker light

---
+++ scenes/entities/flickering_light.gd
@@ -0,0 +1,22 @@
+extends PointLight2D
+
+class_name FlickeringLight
+
+@export_range(0.0, 1.0, 0.01) var frequency:float = 0.3
+@export_range(2.0, 10.0, 1.0) var variation:float = 4.0
+
+@onready var noise = FastNoiseLite.new()
+
+var time_duration: float = 0.0
+var base_energy: float
+
+func _ready() -> void:
+	base_energy = energy;
+	noise.frequency = frequency;
+	
+func _physics_process(delta: float) -> void:
+	time_duration += delta
+	energy = ((noise.get_noise_1d(time_duration) + 1) / variation) + base_energy
+	print(base_energy)
+	print(noise.get_noise_1d(time_duration))
+	print(energy)

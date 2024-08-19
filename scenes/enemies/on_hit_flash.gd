c164a39d168767aaf77413a1e2f0e4182432c9cd
dc950 <dac95@hotmail.co.uk>
2024-08-19 14:03:22 +0100

    Enemies Flashing When Hit

---
+++ scenes/enemies/on_hit_flash.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export var animated_sprite : AnimatedSprite2D
+
+@export var hit_flash_time : float = 0.1
+@export var flash_delay_time : float = 0.2
+
+var hit_flash_timer : float = hit_flash_time
+var ongoing_hit_flash : bool = false
+var flash_delay_timer : float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if animated_sprite and animated_sprite.material:
+		if flash_delay_timer >= 0.0:
+			flash_delay_timer -= delta
+		if ongoing_hit_flash:
+			hit_flash_timer -= delta
+			if hit_flash_timer <= 0.0:
+				animated_sprite.material.set_shader_parameter("Hit", false)
+				hit_flash_timer = hit_flash_time
+				ongoing_hit_flash = false
+
+func _on_health_component_damaged(damage: int) -> void:
+	if animated_sprite and animated_sprite.material:
+		if flash_delay_timer <= 0.0:
+			flash_delay_timer = flash_delay_time
+			ongoing_hit_flash = true
+			animated_sprite.material.set_shader_parameter("Hit", true)

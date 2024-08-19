7a3a302886eda69653e455a36aa31df329133922
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2024-08-19 18:39:01 +0100

    FIX start game button not working

--- scenes/menus/start_menu.gd
+++ scenes/menus/start_menu.gd
@@ -3,7 +3,7 @@ extends ColorRect
 class_name StartMenu
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/environment/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 
 func _on_how_to_button_pressed() -> void:
 	pass # Replace with function body.

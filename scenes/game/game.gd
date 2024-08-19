e9a20df5aea4e2440db358e9d0eef6971f070849
Nicholas Robinson <kylenr23@gmail.com>
2024-08-19 23:04:41 +0100

    FIX remove auto level load in Game Scene

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -2,20 +2,12 @@ extends Node2D
 
 class_name Game
 
-@export var level_1 : PackedScene
-@onready var money_coordinator = $MoneyCoordinator
 @onready var hud = %Hud
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#var level : Level = level_1.instantiate()
-	#self.add_child(level)
 
 func change_level(level: PackedScene):
 	self.add_child(level.instantiate())
 	var level_select = $"LevelSelect"
 	if level_select:
 		level_select.queue_free()
-	
-	money_coordinator.visible = true
+
 	hud.visible = true

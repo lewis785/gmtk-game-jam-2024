b31f35d02160942f4c204ac835aece2994906152
Nicholas Robinson <kylenr23@gmail.com>
2024-08-20 14:17:44 +0100

    ADD Pause Button

--- scenes/menus/pause_menu.gd
+++ scenes/menus/pause_menu.gd
@@ -41,3 +41,7 @@ func _on_main_menu_button_pressed() -> void:
 
 func _on_quit_game_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_pause_button_pressed():
+	if !game_over.game_over:
+		toggle_pause()

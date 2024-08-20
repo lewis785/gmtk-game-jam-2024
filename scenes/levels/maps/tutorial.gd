c570680522579797e77b92e6bcb0689490ab0fe3
dc950 <dac95@hotmail.co.uk>
2024-08-20 13:41:58 +0100

    Add Basic Tutorial

---
+++ scenes/levels/maps/tutorial.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+
+@export var tips : Array[String]
+var tip_index = 0
+
+@onready var tutorial_tip: Label = %TutorialTip
+@onready var skip_button: Button = %SkipButton
+@onready var ok_button: Button = %OkButton
+@onready var tutorial: CanvasLayer = %Tutorial
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_tree().paused = true
+	if tips.size() >= 1:
+		show_tip()
+	else:
+		end_tutorial()
+
+func end_tutorial():
+	tutorial.hide()
+	get_tree().paused = false
+
+func _on_skip_button_pressed() -> void:
+	end_tutorial()
+
+func _on_ok_button_pressed() -> void:
+	tip_index += 1
+	show_tip()
+
+func show_tip():
+	if tip_index >= tips.size():
+		end_tutorial()
+		return
+	tutorial_tip.text = tips[tip_index]
